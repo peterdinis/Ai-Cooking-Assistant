c67d277af3f73e27dec5015656b59f65654164f9
peterdinis <petter1@pobox.sk>
2026-03-15 15:41:45 +0100

    feat: New hooks folder

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,16 +1,6 @@
+import { useIsClient } from "@/hooks/useIsClient";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes";
-import { useEffect, useState } from "react";
-
-function useIsClient() {
-	const [isClient, setIsClient] = useState(false);
-
-	useEffect(() => {
-		setIsClient(true);
-	}, []);
-
-	return isClient;
-}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
 	const isClient = useIsClient();
